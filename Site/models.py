2bca414d1ebedf2e991bfc58d7db817213330394
Maria Tauany Santos Feitosa <tauany.feitosa@lais.huol.ufrn.br>
2022-12-01 14:26:47 -0300

    add: base da documentação do django para a criação de usuários e administradores

--- Site/models.py
+++ Site/models.py
@@ -1,3 +1,59 @@
+from django.contrib.auth.models import BaseUserManager
 from django.db import models
 
-# Create your models here.
+class GerenciadorUsuarios(BaseUserManager):
+    def create_user(self, matricula, cpf, nome_completo, data_nascimento, titulacao, universidade, endereco, bairro,
+                    municipio, cep, termos_uso, membro_lipe, password=None, **outros_campos):
+        if not cpf or not matricula:
+            raise ValueError('Membros da LIPE devem ter um CPF válido.')
+
+        user = self.model(
+            cpf=cpf,
+            matricula=matricula,
+            nome_completo=nome_completo,
+            data_nascimento=data_nascimento,
+            titulacao=titulacao,
+            universidade=universidade,
+            endereco=endereco,
+            bairro=bairro,
+            municipio=municipio,
+            cep=cep,
+            termos_uso=termos_uso,
+            membro_lipe=membro_lipe
+            **outros_campos
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, matricula, cpf, nome_completo, data_nascimento, titulacao, universidade, endereco, bairro,
+                    municipio, cep, termos_uso, membro_lipe, password=None, **outros_campos ):
+
+        outros_campos.setdefault('is_superuser', True)
+        outros_campos.setdefault('is_staff', True)
+        outros_campos.setdefault('is_active', True)
+
+        if outros_campos.get('is_superuser') == False:
+            raise ValueError('Um Superusuário deve ter "is_superuser" definido como "True".')
+
+        if outros_campos.get('is_staff') == False:
+            raise ValueError('Um Superusuário deve ter "is_staff" definido como "True".')
+
+        user = self.model(
+            cpf=cpf,
+            matricula=matricula,
+            nome_completo=nome_completo,
+            data_nascimento=data_nascimento,
+            titulacao=titulacao,
+            universidade=universidade,
+            endereco=endereco,
+            bairro=bairro,
+            municipio=municipio,
+            cep=cep,
+            termos_uso=termos_uso,
+            membro_lipe=membro_lipe
+                        ** outros_campos
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
